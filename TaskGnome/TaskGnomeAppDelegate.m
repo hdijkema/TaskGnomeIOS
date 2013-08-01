385783b09facb75eb8d5da81e168c4735a384c84
Lion User <User@Lions-Mac.local>
2013-08-01 16:40:21 +0200

    GPL Notice

--- TaskGnome/TaskGnomeAppDelegate.m
+++ TaskGnome/TaskGnomeAppDelegate.m
@@ -1,10 +1,24 @@
-//
-//  TestAppDelegate.m
-//  Test
-//
-//  Created by Lion User on 09/07/2013.
-//  Copyright (c) 2013 Covalent. All rights reserved.
-//
+/**********************************************************************
+ TaskGnomeAppDelegate.m
+ 
+ This file is part of TaskGnome IOS.
+ 
+ TaskGnome is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Foobar is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with TaskGnome IOS.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ Copyright (c) 2013 Hans Oesterholt. All rights reserved.
+ **********************************************************************/
+
 
 #import "TaskGnomeAppDelegate.h"
 
